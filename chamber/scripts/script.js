baef29db74c8ac6715483be6849a349d0b228957
acconnitt <acconnitt@gmail.com>
2022-10-14 21:11:23 -0400

    final 4

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,4 @@
+// OPEN AND CLOSE MENU 
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -10,6 +11,7 @@ x.onclick = toggleMenu;
 
 
 
+// DATE FOR FOOTER
 // Select the HTML element to manipulate
 const year1 = document.querySelector("#year1")
 const date1 = document.querySelector("#date1");
@@ -26,15 +28,36 @@ try {
 
     year1.innerHTML = `Today is <span class="highlight">${new Date().toLocaleDateString("en-GB", options.year)}</span>`;
     date1.innerHTML = `Last Modification: <span class="highlight">${new Date().toLocaleDateString("en-GB", options)}</span>`;
-    
 } 
 
 catch (e) {
     alert("Error with code or your browser does not support Locale");
 }
 
-// Long hand method ... building day and month names from built-in date methods.
 
+// DATE FOR HEADER
+const date2 = document.querySelector("#date2");
+try {
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+
+    date2.innerHTML = `<span class="highlight">${new Date().toLocaleDateString("en-GB", options)}</span>`;
+}
+
+catch (e) {
+    alert("Error with code or your browser does not support Locale");
+}
+
+
+
+
+
+// Long hand method ... building day and month names from built-in date methods.
 const daynames = [
     "Sunday",
     "Monday",
@@ -64,3 +87,30 @@ const monthName = months[d.getMonth()];
 const year = d.getFullYear();
 const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 document.querySelector("#year1").textContent = `${year}`;
+
+
+
+// var currentDate = new Date();
+// var months = currentDate.getMonth();
+// var days = currentDate.getDate();
+// var years = currentDate.getFullYear();
+// var fullDate = months + '-' + days + '-' + years;
+// document.write(fullDate());
+
+
+// const timeElement = document.querySelector(".date")
+
+// //
+// function formatDate(date) {
+//     const days = [
+//         "Sunday",
+//         "Monday",
+//         "Tuesday",
+//         "Wednesday",
+//         "Thursday",
+//         "Friday",
+//         "Saturday"
+// }
+
+
+
