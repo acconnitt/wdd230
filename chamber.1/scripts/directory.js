83870ee8496fb9313361b0d4561a701e2afd3ff6
acconnitt <acconnitt@gmail.com>
2022-11-30 07:45:23 -0500

    more

--- chamber.1/scripts/directory.js
+++ chamber.1/scripts/directory.js
@@ -47,3 +47,20 @@ function displayBusinesses(business) {
 }
 
 getBusinessCard();
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
