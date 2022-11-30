e4c1c3e74b7f62978f867aca2d590f76e22e95ef
acconnitt <acconnitt@gmail.com>
2022-11-30 00:51:17 -0500

    more

--- chamber.1/scripts/directory.js
+++ chamber.1/scripts/directory.js
@@ -1,74 +1,49 @@
-const requestURL = './data.json';
-
-// fetch('https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json')
-// .then((response) => response.json())
-// .then((json) => console.log(json));
-
-
-// const requestURL = $.getJSON('./data');
-// $.getJSON('./data.json', function(data));
-// const requestURL = requests.get('./data.json');
-// import requestURL from './data.json';
-// const requestURL = JSON.parse('./data.json')
-// const requestURL = 'data.json';
-
+const requestFile = 'scripts/data.json';
 const cards = document.querySelector('.cards');
 
-
 // More robust way 
-async function getProphets() {
-    const response = await fetch(requestURL);
+async function getBusinessCard() {
+    const response = await fetch(requestFile);
     //Convert to json 
     const jsonObject = await response.json();
-    const prophets = jsonObject['prophets'];
-    // console.log(prophets);
-    console.table(jsonObject);
-    prophets.forEach(displayProphets);
+    const business = jsonObject['businessCards'];
+    // console.log(business);
+    // console.table(jsonObject);
+    business.forEach(displayBusinesses);
 }
 
+function displayBusinesses(business) {
 
-
-function displayProphets(prophet) {
-
-    // Create elements to add to the document
-    // Created a section, h2, h3, and image
     let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let h3 = document.createElement('h3');
-    // let p = document.createElement('p');
     let portrait = document.createElement('img');
-    let birthLoc = document.createElement('p');
-    let dob = document.createElement('p');
-    let children = document.createElement('p');
+    let title = document.createElement('h2');
+    let phone = document.createElement('p');
+    let address = document.createElement('p');
+    let website = document.createElement('p');
+    
 
-    [birthLoc, dob, children].forEach((element) => {
+    [title, phone, address, website].forEach((element) => {
         element.classList.add('caption');
     })
 
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    // h2.textContent = prophet.name + ' ' + prophet.lastname;
-    //same as bellow
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    h3.textContent = `Place of birth: ${prophet.birthplace}`
-    birthLoc.textContent = `${"Place of Birth:"} ${prophet.birthplace}`
-    dob.textContent = `${"Date of Birth:"} ${prophet.birthdate}`;
-    children.textContent = `${"Number of Children:"} ${prophet.numofchildren}`;
+    
+    title.textContent = `${business.name}`;
+    phone.textContent = `${business.phone}`
+    website.textContent = `${business.website}`
+    address.textContent = `${business.address}`;
 
 
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+    // Build the image attributes by using the setAttribute method for the src, alt, 
+    // and loading attribute values. (Fill in the blank with the appropriate variable).
+    portrait.setAttribute('src', business.imageurl);
+    portrait.setAttribute('alt', `Portait of ${business.name} ${business.lastname}`);
     portrait.setAttribute('loading', 'lazy');
 
-    // // Add/append the section(card) with the h2 element
-    // card.appendChild(h2);
-    // card.appendChild(portrait);
-
     // Add/append the existing HTML div with the cards class with the section(card)
-    card.append(h2, dob, birthLoc, portrait);
+    card.append(title, portrait, phone, address, website);
 
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
 }
 
-getProphets();
+getBusinessCard();
--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -1,14 +1,9 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+// ********* URL ********* //
+// const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+// ********* OR LOCALY ********* //
+const requestURL = 'scripts/prophets2.json';
 const cards = document.querySelector('.cards');
-
-// fetch(requestURL)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (jsonObject) {
-//     console.table(jsonObject);  // temporary checking for valid response and data parsing
-//   });
-
 
 // More robust way 
 async function getProphets() {
